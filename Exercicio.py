25984311adc250c448357ee871fa62008186283c
Acg - David Ribeiro Ferreira <acg.ferreira@tigre.com>
2022-06-08 15:28:46 -0300

    first commit

---
+++ Exercicio.py
@@ -0,0 +1,26 @@
+#Python 05/05/2022
+
+#Variaveis
+a= 1
+b = 2
+
+#Indexing
+#variavel1 = 'TesteFuncional'
+# 0 primeiro caracter
+# -1 Ultimo
+
+#print(variavel1[-1]) 
+
+#Slicing
+variavel1 = 'TesteFuncional'
+print(variavel1[0:2]) # do zero ao 2 , segundo nao incluso
+print(variavel1[:3]) # todos ate o terceiro, terceiro nao incluso
+print(variavel1[0:3] + variavel1[3:]) # inicio ate o 3 e do 3 ate o fim
+
+palavra = 'umapalavrasupergrande'
+print(len(palavra)) #tamanho da string
+print(palavra [: -1]) # palavra - 1 letra
+
+print('sao paulo'.title())
+
+#print(variavel1[0]) 
